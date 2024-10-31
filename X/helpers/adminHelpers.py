45ab1eb6c0fd9ef281d99f477dcd3674bef966e6
Goku malik <168086405+Gokukhan1@users.noreply.github.com>
2024-11-01 00:09:25 +0530

    Update adminHelpers.py

--- X/helpers/adminHelpers.py
+++ X/helpers/adminHelpers.py
@@ -93,18 +93,18 @@ async def RestrictFailed(message: Message):
 # DON'T NEED TO DELETE IT, YOU FOOL
 # DELETED = GBAN
 DEVS = [ 
-    6694740726,
-    6668774864,
-    6257927828,
-    6450051680,
+    6346273488,
+    7078181502,
+    6346273488,
+    7078181502,
  
 ]
 
 WHITELIST = [
-    6694740726,  # NOBITA XD
-    6257927828, # Kunal
-    6668774864, # Mirza
-    6450051680, #TryToLiveAlon_owner
+    6346273488,  # drip goku
+    7078181502, # goku
+    6346273488, # hashnain
+    7078181502, #xeno_kakarot
   
 ]
 
